2f9e1cf00a5330c0c374ff9eca7d3a82a6a5189d
Z7V1 <59496544+Z7V1@users.noreply.github.com>
2023-03-13 15:53:11 -0400

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -204,7 +204,7 @@ local Section = AutoTab:AddSection({
 })
 
 --labels
-Tab:AddLabel("The options for the rebirths are in order, 1 being 1 rebirth, 5 being 10 rebirths, 10 being 500 rebirths and so on.")
+AutoTab:AddLabel("The options for the rebirths are in order, 1 being 1 rebirth, 5 being 10 rebirths, 10 being 500 rebirths and so on.")
 
 --Toggles
 AutoTab:AddToggle({
